bbcc28dd71c08380559e659df7911a113a6da3c8
Matthew Rawson <mrawson@redev.co.uk>
2024-02-21 17:35:00 +0000

    Updated to lock the size at 460x640

--- flutternotify/lib/main.dart
+++ flutternotify/lib/main.dart
@@ -1,10 +1,19 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'presentation/app.dart';
+import 'package:bitsdojo_window/bitsdojo_window.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
 
   if (Platform.isWindows || Platform.isMacOS) {}
   runApp(const App());
+
+  doWhenWindowReady(() {
+    const initialSize = Size(460, 640);
+    appWindow.minSize = initialSize;
+    appWindow.size = initialSize;
+    appWindow.maxSize = initialSize;
+    appWindow.show();
+  });
 }
