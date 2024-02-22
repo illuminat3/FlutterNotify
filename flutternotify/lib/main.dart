c960a8a37110242cb5e4dfb331ecf6f068a63f44
Matthew Rawson <mrawson@redev.co.uk>
2024-02-22 09:46:45 +0000

    Added System Tray Integration

--- flutternotify/lib/main.dart
+++ flutternotify/lib/main.dart
@@ -4,8 +4,32 @@ import 'presentation/app.dart';
 import 'package:bitsdojo_window/bitsdojo_window.dart';
 import 'package:system_tray/system_tray.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  final systemTray = SystemTray();
+  final Menu menu = Menu();
+
+  await systemTray.initSystemTray(
+    iconPath: getTrayImagePath('chatwoot-logo'),
+  );
+
+  await menu.buildFrom([
+    MenuItemLabel(label: "Show", onClicked: (menuItem) => appWindow.show()),
+    MenuItemLabel(label: "Hide", onClicked: (menuItem) => appWindow.hide()),
+    MenuItemLabel(label: "Exit", onClicked: (menuItem) => exit(0))
+  ]);
+
+  await systemTray.setContextMenu(menu);
+
+  systemTray.registerSystemTrayEventHandler((eventName) {
+    debugPrint("eventName: $eventName");
+    if (eventName == kSystemTrayEventClick) {
+      appWindow.show();
+    } else if (eventName == kSystemTrayEventRightClick) {
+      systemTray.popUpContextMenu();
+    }
+  });
 
   if (Platform.isWindows || Platform.isMacOS) {}
   runApp(const App());
@@ -17,4 +41,8 @@ void main() {
     appWindow.maxSize = initialSize;
     appWindow.show();
   });
+}
+
+String getTrayImagePath(String imageName) {
+  return Platform.isWindows ? 'assets/$imageName.ico' : 'assets/$imageName.png';
 }
