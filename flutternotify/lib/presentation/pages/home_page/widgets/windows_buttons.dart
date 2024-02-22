2b995c64fadef0092995e0176bb6c9e683ea4e58
Matthew Rawson <mrawson@redev.co.uk>
2024-02-22 10:33:51 +0000

    Updated Windows Button to hide on click instead of minimize to taskbar

--- flutternotify/lib/presentation/pages/home_page/widgets/windows_buttons.dart
+++ flutternotify/lib/presentation/pages/home_page/widgets/windows_buttons.dart
@@ -1,14 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:bitsdojo_window/bitsdojo_window.dart';
+import '../../../themes/default/theme.dart';
 
 class WindowButtons extends StatelessWidget {
   const WindowButtons({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final windowButtonColors = WindowButtonColors(
+      iconNormal: DefaultTheme.defaultTheme.colorScheme.onSurface,
+      mouseOver: DefaultTheme.defaultTheme.colorScheme.onPrimary,
+      mouseDown: DefaultTheme.defaultTheme.colorScheme.primary,
+    );
+
     return Row(
       children: [
-        MinimizeWindowButton(),
+        WindowButton(
+          colors: windowButtonColors,
+          iconBuilder: (buttonContext) =>
+              MinimizeIcon(color: buttonContext.iconColor),
+          onPressed: appWindow.hide,
+        ),
         CloseWindowButton(),
       ],
     );
