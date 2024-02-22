1d2f91c8b0098effcb454c0ed39aa91d4c054bc2
Matthew Rawson <mrawson@redev.co.uk>
2024-02-22 11:01:30 +0000

    Refactored to use the new custom buttons

--- flutternotify/lib/presentation/pages/home_page/widgets/windows_buttons.dart
+++ flutternotify/lib/presentation/pages/home_page/widgets/windows_buttons.dart
@@ -1,29 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:bitsdojo_window/bitsdojo_window.dart';
-import '../../../themes/default/theme.dart';
+import 'minimize_window_button.dart';
+import 'close_window_button.dart';
 
 class WindowButtons extends StatelessWidget {
   const WindowButtons({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final windowButtonColors = WindowButtonColors(
-      iconNormal: DefaultTheme.defaultTheme.colorScheme.onSurface,
-      mouseOver: DefaultTheme.defaultTheme.colorScheme.onPrimary,
-      mouseDown: DefaultTheme.defaultTheme.colorScheme.primary,
-    );
-
     return Row(
       children: [
-        WindowButton(
-          colors: windowButtonColors,
-          iconBuilder: (buttonContext) =>
-              MinimizeIcon(color: buttonContext.iconColor),
-          onPressed: appWindow.hide,
-        ),
-        CloseWindowButton(
-          colors: windowButtonColors,
-        ),
+        MinWindowButton(),
+        ClsWindowButton(),
       ],
     );
   }
